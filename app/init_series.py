6635252242dc328ab59d4be9487838c1a0da2cd7
70425515249 <alex.pereira@planejamento.gov.br>
2019-02-19 10:10:09 -0300

    altere o script de inicializacao dos dados

--- app/init_series.py
+++ app/init_series.py
@@ -4,4 +4,4 @@ init_date_str = '2019-01-01'
 final_date_str = '2020-12-31'
 n_servicos = 900
 
-create_initial_data('./app/progress.npy', n_servicos, init_date_str, final_date_str)
+create_initial_data('./progress.npy', n_servicos, init_date_str, final_date_str)
